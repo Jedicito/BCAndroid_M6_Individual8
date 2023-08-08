6de791c7e6828687c602e7e6feb18fef888d9592
Jedicito <seron.cesar@gmail.com>
2023-08-07 22:11:55 -0400

    Repositorio

--- app/src/main/java/chl/ancud/m6_individual8/data/Repositorio.kt
+++ app/src/main/java/chl/ancud/m6_individual8/data/Repositorio.kt
@@ -1,4 +1,22 @@
 package chl.ancud.m6_individual8.data
 
-class Repositorio {
+import chl.ancud.m6_individual8.data.local.RazaDao
+import chl.ancud.m6_individual8.data.local.RazaEntity
+import chl.ancud.m6_individual8.data.remote.Raza
+import chl.ancud.m6_individual8.data.remote.RazaAPI
+import retrofit2.Response
+
+class Repositorio(private val razaAPI: RazaAPI, private val razaDao: RazaDao) {
+
+    suspend fun getRazas() {
+        val response = razaAPI.getData() //acá llegan los datos
+        if(response.isSuccessful) {  //Llegaron los datos?
+            val message = response.body()!!.message //Solo sacando la parte de message, sin status
+            val keys = message.keys
+            keys.forEach {
+                val razaEntity = RazaEntity(it)
+                razaDao.insertRaza(razaEntity)
+            }
+        }
+    }
 }
