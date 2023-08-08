01fa6435bd1953ddb8b29bd4656b5d07c12d84aa
Jedicito <seron.cesar@gmail.com>
2023-08-07 22:15:04 -0400

    ModelView

--- app/src/main/java/chl/ancud/m6_individual8/presentacion/RazaViewModel.kt
+++ app/src/main/java/chl/ancud/m6_individual8/presentacion/RazaViewModel.kt
@@ -1,4 +1,24 @@
 package chl.ancud.m6_individual8.presentacion
 
-class RazaViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import chl.ancud.m6_individual8.data.Repositorio
+import chl.ancud.m6_individual8.data.local.RazaDatabase
+import chl.ancud.m6_individual8.data.remote.RazaRetrofit
+import kotlinx.coroutines.launch
+
+class RazaViewModel (application: Application): AndroidViewModel(application) {
+    private val repositorio: Repositorio
+
+    init {
+        val api = RazaRetrofit.getRetrofitRaza()
+        val razaDatabase = RazaDatabase.getDatabase(application).getRazaDao()
+        repositorio = Repositorio(api, razaDatabase)
+    }
+
+    fun getAllRazas() = viewModelScope.launch {
+        repositorio.getRazas()
+    }
+
 }
